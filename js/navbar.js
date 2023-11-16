b3156e1f3307b3edc9129a1f697ef9561167e494
zumaku <60200121101@uin-alauddin.ac.id>
2023-11-16 12:08:08 +0800

    Menyelesaikan navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,8 @@
 const navbar = document.querySelector("#navbar");
+const humb = document.querySelector(".humb");
+const back = document.querySelector("#back");
+const menus = document.querySelector(".menus");
 
+humb.addEventListener("click", () => {if(menus.classList.contains("menuopen"))menus.classList.remove("menuopen")})
+back.addEventListener("click", () => {if(!menus.classList.contains("menuopen"))menus.classList.add("menuopen")})
 
