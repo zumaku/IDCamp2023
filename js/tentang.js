4190fb22ba0bcc075dde32df179dde506c4cfee5
zumaku <60200121101@uin-alauddin.ac.id>
2023-12-24 14:25:18 +0800

    Menyelesaikan Dasar Pemrograman Web

--- js/tentang.js
+++ js/tentang.js
@@ -2,8 +2,5 @@ const aside = document.querySelector(".biodata")
 const tombol = document.querySelector(".tombol")
 
 tombol.addEventListener("click", () => {
-    // if(aside.classList.contains("muncul")){
-
-    // }
     aside.classList.toggle("muncul")
 })
