360dd88f971e84a60c1d9a2c2e525789d7bc2203
zumaku <60200121101@uin-alauddin.ac.id>
2023-12-24 14:32:01 +0800

    Extrak starter kit yang diberikan

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.querySelector("#navbar");
-const humb = document.querySelector(".humb");
-const back = document.querySelector("#back");
-const menus = document.querySelector(".menus");
-
-humb.addEventListener("click", () => {if(menus.classList.contains("menuopen"))menus.classList.remove("menuopen")})
-back.addEventListener("click", () => {if(!menus.classList.contains("menuopen"))menus.classList.add("menuopen")})
-
