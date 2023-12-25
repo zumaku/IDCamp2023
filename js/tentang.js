4e5b3c58e8f3308b3eda35cc7fbb44ad13624712
zumaku <60200121101@uin-alauddin.ac.id>
2023-12-25 19:41:48 +0800

    Prepear to install react

--- js/tentang.js
+++
@@ -1,6 +0,0 @@
-const aside = document.querySelector(".biodata")
-const tombol = document.querySelector(".tombol")
-
-tombol.addEventListener("click", () => {
-    aside.classList.toggle("muncul")
-})
